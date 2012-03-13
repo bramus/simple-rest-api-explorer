fe71d58cd73c93cc95e51597d266a0c53fc263b5
Bramus <bramus@bram.us>
2012-03-13 12:29:43 +0100

    Enabled history.pushState();

--- js/main.js
+++ js/main.js
@@ -22,28 +22,40 @@ jQuery(function($) {
 	}
 	
 	// hook sidebar clicks
-	$('#sidebar').on('click', 'a', function(e) {
+	$('#sidebar').on('click', 'a', function(e, dontpush) {
 		e.preventDefault();
 		if ($('#loading').is(':hidden')) {
 			$('#sidebar a').removeClass('active');
 			$(this).addClass('active');
-			$('#apiurl').val(apiBaseUrl + $(this).attr('href'));
-			makeCall(apiBaseUrl + $(this).attr('href'), $(this).data('requestmethod') || 'get', $(this).data('extradata') || {});
+			var url = $(this).attr('href');
+			$('#apiurl').val(apiBaseUrl + url);
+			!dontpush && history.pushState({'href': url}, 'clicked ' + url, '#' + url);
+			makeCall(apiBaseUrl + url, $(this).data('requestmethod') || 'get', $(this).data('extradata') || {});
 		}
 	});
 		
 	// don't send forms
 	$('form').on('submit', function(e){
 		e.preventDefault();
-		
 		if ($('#apiurl').val().indexOf(apiBaseUrl) == 0)
 			makeCall($('#apiurl').val());
 		else
 			alert('You are only allowed make calls to ' + apiBaseUrl);
-			
 	});
 	
-	// prefill on load
-	$('#sidebar a:first').trigger('click');
+	// History Popstate
+	$(window).on('popstate', function(e) {
+		e.preventDefault();
+		$('#sidebar a').removeClass('active');
+		if (e.originalEvent.state && e.originalEvent.state.href) {
+			$('#sidebar a[href="' + e.originalEvent.state.href + '"]').trigger('click', true);
+		} else {
+			if (window.location.hash) {
+				$('#sidebar a[href="' + window.location.hash.substr(1) + '"]').trigger('click', true);
+			} else {
+				$('#sidebar a:first').addClass('active').trigger('click', true);
+			}
+		}
+	});
 	
 });
